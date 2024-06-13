d5311b22d8b8195101d02eaac12d35634d21a1fd
jjg0111 <jjgfbi@datagrab.co.kr>
2024-06-13 17:35:15 +0900

    게임 목록 수정

--- src/app/components/gamelist/GameCard.js
+++ src/app/components/gamelist/GameCard.js
@@ -2,11 +2,15 @@
 import React from "react";
 
 export default function GameCard({ props, className }) {
+    const data = props;
+    console.log(data.img)
     return (
-        <div className={`inline-block ${className} desktop:max-w-[372px] `}>
-            <div className={`relative desktop:max-h-[182px] mobile:max-h-[171px] overflow-hidden rounded-lg bg-center`}>
-                <div></div>
-                <h1>{props.name}</h1>
+        <div className={`inline-block ${className} max-w-[372px] `}>
+            <div className={`relative max-h-[182px] bg-slate-400 overflow-hidden rounded-lg bg-center flex flex-col`}>
+                <div className="overflow-hidden flex-1">
+                    <img src={data.img}  />
+                </div>
+                <h1 className="text-center">{data.name}</h1>
             </div>
         </div>
     )
--- src/app/gamelist/page.js
+++ src/app/gamelist/page.js
@@ -1,27 +1,28 @@
 "use client"
 import Image from "next/image";
 import gamelist from "@/app/components/gamelist/gamelist.json"
-import GameCard from "../components/gamelist/gamecard";
+import GameCard from "../components/gamelist/GameCard";
 
 export default function page() {
-  console.log(gamelist)
   const data = gamelist;
   let content = (
     <div className="">
       {data?.map((item, index) => (
-        <GameCard props={item} key={"game"+index} className={index % 2 == 0 && "desktop:mr-4"} />
+        <GameCard props={item} key={"game"+index} className={index % 2 == 0 && "mb-4 mr-4"} />
       ))}
     </div>
   )
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="w-full min-h-screen bg-white h-full">
+    <main className="flex min-h-screen flex-col">
+      <div className="w-full min-h-screen bg-white h-full item-center">
         {/* <img src="https://storage.googleapis.com/playtablestorage/siteimg/home-main.jpg" className="max-w-[800px] p-5 w-full text-center" /> */}
-        <div className="w-full p-5 text-center">
-          <h1 className="text-xl ">플레이테이블에서 플레이 가능한 게임은 다음과 같습니다.</h1>
+        <div className="w-full p-5 text-center space-y-4">
+          <h1 className="text-xl">플레이테이블에서 플레이 가능한 게임은 다음과 같습니다.</h1>
+          <div className="text-left">
           {content}
+          </div>
         </div>
-        {/* <ResultList /> */}
+        
       </div>
     </main>
   );
