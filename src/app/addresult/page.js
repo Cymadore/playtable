bc647b055ab161400721e8c5344421eac63fbc3d
jjg0111 <jjgfbi@datagrab.co.kr>
2024-07-31 16:43:30 +0900

    수정

--- src/app/addresult/page.js
+++ src/app/addresult/page.js
@@ -110,54 +110,54 @@ export default function Page() {
             </div>
           </div>
           {game&&(
-            <div className="w-full rounded-2xl p-5 bg-gray-200 shadow-lg">
-              <div className="cursor-pointer">
-                <div className="flex space-x-2">
-                  <IoSearch size={30} />
-                  <div className="relative w-full flex-1">
-                  <input
-                    type="text"
-                    onChange={(e) => setP1FactionKeyword(e.target.value)}
-                    value={p1FactionKeyword}
-                    placeholder="자신의 팩션을 선택해주세요"
-                    className=" w-full rounded-lg bg-slate-100 !outline-none text-2xl indent-4 text-black"
-                    onClick={()=>setIsP1Faction(!isP1Faction)}
-                    // onKeyDown={handleKeyDown}
-                  />
-                  {isP1Faction&&(
-                  <div className="px-5 border-black border-2 w-full max-h-[180px] bg-slate-100 absolute overflow-scroll no-scrollbar space-y-2 z-20">
-                    {filteredP1F?.map((item, index)=>(
-                      <div className="" key={'gamelist'+index} onClick={()=>selectP1Faction(item)}>{item.name}</div>
-                    ))}
-                  </div>
-                  )}
+            <div className="space-y-5">
+              <div className="w-full rounded-2xl p-5 bg-gray-200 shadow-lg">
+                <div className="cursor-pointer">
+                  <div className="flex space-x-2">
+                    <IoSearch size={30} />
+                    <div className="relative w-full flex-1">
+                    <input
+                      type="text"
+                      onChange={(e) => setP1FactionKeyword(e.target.value)}
+                      value={p1FactionKeyword}
+                      placeholder="자신의 팩션을 선택해주세요"
+                      className=" w-full rounded-lg bg-slate-100 !outline-none text-2xl indent-4 text-black"
+                      onClick={()=>setIsP1Faction(!isP1Faction)}
+                      // onKeyDown={handleKeyDown}
+                    />
+                    {isP1Faction&&(
+                    <div className="px-5 border-black border-2 w-full max-h-[180px] bg-slate-100 absolute overflow-scroll no-scrollbar space-y-2 z-20">
+                      {filteredP1F?.map((item, index)=>(
+                        <div className="" key={'gamelist'+index} onClick={()=>selectP1Faction(item)}>{item.name}</div>
+                      ))}
+                    </div>
+                    )}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          )}
-          {game&&(
-            <div className="w-full rounded-2xl p-5 bg-gray-200 shadow-lg">
-              <div className="cursor-pointer">
-                <div className="flex space-x-2">
-                  <IoSearch size={30} />
-                  <div className="relative w-full flex-1">
-                  <input
-                    type="text"
-                    onChange={(e) => setP2FactionKeyword(e.target.value)}
-                    value={p2FactionKeyword}
-                    placeholder="상대의 팩션을 선택해주세요"
-                    className=" w-full rounded-lg bg-slate-100 !outline-none text-2xl indent-4 text-black"
-                    onClick={()=>setIsP2Faction(!isP2Faction)}
-                    // onKeyDown={handleKeyDown}
-                  />
-                  {isP2Faction&&(
-                  <div className="px-5 border-black border-2 w-full max-h-[180px] bg-slate-100 absolute overflow-scroll no-scrollbar space-y-2 z-20">
-                    {filteredP2F?.map((item, index)=>(
-                      <div className="" key={'gamelist'+index} onClick={()=>selectP2Faction(item)}>{item.name}</div>
-                    ))}
-                  </div>
-                  )}
+              <div className="w-full rounded-2xl p-5 bg-gray-200 shadow-lg">
+                <div className="cursor-pointer">
+                  <div className="flex space-x-2">
+                    <IoSearch size={30} />
+                    <div className="relative w-full flex-1">
+                    <input
+                      type="text"
+                      onChange={(e) => setP2FactionKeyword(e.target.value)}
+                      value={p2FactionKeyword}
+                      placeholder="상대의 팩션을 선택해주세요"
+                      className=" w-full rounded-lg bg-slate-100 !outline-none text-2xl indent-4 text-black"
+                      onClick={()=>setIsP2Faction(!isP2Faction)}
+                      // onKeyDown={handleKeyDown}
+                    />
+                    {isP2Faction&&(
+                    <div className="px-5 border-black border-2 w-full max-h-[180px] bg-slate-100 absolute overflow-scroll no-scrollbar space-y-2 z-20">
+                      {filteredP2F?.map((item, index)=>(
+                        <div className="" key={'gamelist'+index} onClick={()=>selectP2Faction(item)}>{item.name}</div>
+                      ))}
+                    </div>
+                    )}
+                    </div>
                   </div>
                 </div>
               </div>
