4fd89f5a08cadbb3448c5f3f77e3c4bb5238020e
jjg0111 <jjgfbi@datagrab.co.kr>
2024-09-24 14:29:15 +0900

    이미지 CORS 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'storage.googleapis.com', // Google Cloud Storage 도메인
+            port: '', // 포트는 비워둡니다
+            pathname: '/playtable-photo', // 버킷 이름과 이미지 경로
+          },
+        ],
+      },
+};
 
 export default nextConfig;
