5a8f4c2957ca1e95fbb4ee333203789588d233fe
cymadore <jjg0111@naver.com>
2024-09-24 22:18:13 +0900

    tnwjd

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
             protocol: 'https',
             hostname: 'storage.googleapis.com', // Google Cloud Storage 도메인
             port: '', // 포트는 비워둡니다
-            pathname: '/playtable-photo', // 버킷 이름과 이미지 경로
+            pathname: '/playtable-photo', 
           },
         ],
       },
