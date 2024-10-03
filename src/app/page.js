a00de2311f126d2044490dc9d3906afa0bef327b
jjg0111 <jjg0111@naver.com>
2024-10-03 17:05:59 +0900

    123

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="w-full min-h-screen bg-gray-100 h-full">
         <img src="https://storage.googleapis.com/playtable-photo/siteimg/Main.webp" className="max-w-[800px] p-5 w-full text-center" />
         <div className="w-full p-5 text-center">
-          <h1 className="text-xl text-gray-900">플레이테이블은 청량리에 위치한 미니어쳐 게임 클럽입니다.</h1>
+          <h1 className="text-xl text-gray-900">플레이테이블은 청량리에 위치한 미니어쳐 게임 클럽입니다.123</h1>
           <h1 className="m-5 text-gray-900">플레이테이블은 2023년 10월 제기동에 오픈한 미니어쳐 게임 클럽입니다.<br/>다양한 미니어쳐 게임을 플레이 할 수 있는 공간과 페인팅을 할 수 있는 공간이 준비되어 있습니다.</h1>
         </div>
       </div>
